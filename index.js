0a7f8ab187c54eeabb8fcd560c4366da4e49bd33
Grant Glidewell <ggcadc@gmail.com>
2018-07-07 19:02:55 -0700

    this is now an attempt to use nextjs for ssr react

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
-const cheerio = require("cheerio");
-const fetch = require("node-fetch");
-const AWS = require("aws-sdk");
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
 
-const sites = require("./sites");
-const config = require("./config");
-
-const s3 = new AWS.S3(config);
+const sites = require('./sites.json')
 
 async function getCoffee(site) {
-  const page = await fetch(`${sites[site].url}`).then(res => res.text());
+  const page = await fetch(`${sites[site].url}`).then(res => res.text())
   const $ = cheerio.load(page, {
     withDomLvl1: true,
     normalizeWhitespace: false,
     xmlMode: true,
     decodeEntities: true
-  });
-  let selected = $(`.${sites[site].class}`).text();
-  return { [site]: selected.replace(/\s+/g, " ") };
+  })
+  let selected = $(`.${sites[site].class}`).text()
+  return { [site]: selected.replace(/\s+/g, ' ') }
 }
 
-const getAllCoffees = Promise.all(
-  Object.keys(sites).map(site => getCoffee(site))
-)
+const coffeeScrape = Promise.all(Object.keys(sites).map(site => getCoffee(site)))
   .then(allCoffeeData => {
-    s3
-      .putObject({
-        Bucket: "gg-nodejs-misc",
-        Key: "coffeeOfferings/coffee.json",
-        Body: JSON.stringify(allCoffeeData),
-        ContentType: "application/json",
-        ACL: "public-read"
-      })
-      .promise();
+    allCoffeeData.map(x => console.log(JSON.stringify(x, null, 2)))
   })
-  .then(console.log)
-  .catch(console.log);
+  .catch(console.error)
 
-exports.handler = getAllCoffees;
