dc834bc6f7848ad97fe896e57ec71e2ef2d176f4
Matthew Grill <matt.grill@acquia.com>
2018-11-20 21:11:16 -0800

    fixes for style, consistency

--- pages/addsite.js
+++ pages/addsite.js
@@ -1,25 +1,23 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-export default () => {
-  return (
-    <section>
-      <form>
-        <label>site name</label>
-        <input type="text" name="name" />
-        <br />
-        <label>site url</label>
-        <input type="text" name="url" />
-        <br />
-        <label>site selector</label>
-        <input type="text" name="name" />
-      </form>
-      <Link href="/">
-        <a>home</a>
-      </Link>
+export default () => (
+  <section>
+    <form>
+      <label>site name</label>
+      <input type="text" name="name" />
       <br />
-      <Link href="/sites">
-        <a>sites</a>
-      </Link>
-    </section>
-  )
-}
+      <label>site url</label>
+      <input type="text" name="url" />
+      <br />
+      <label>site selector</label>
+      <input type="text" name="name" />
+    </form>
+    <Link href="/">
+      <a>home</a>
+    </Link>
+    <br />
+    <Link href="/sites">
+      <a>sites</a>
+    </Link>
+  </section>
+);
--- pages/sites.js
+++ pages/sites.js
@@ -1,27 +1,31 @@
-import Link from 'next/link'
-const sites = require('../sites.json')
+import Link from 'next/link';
 
-export default () => {
-  return (
-    <div>
-      <form>
-        {Object.keys(sites).map(site => {
-          return <section key={site}>
+const sites = require('../src/sites.json');
+
+export default () => (
+  <div>
+    <form>
+      {Object.keys(sites).map(site => {
+        return (
+          <section key={site}>
             <label>{site}</label>
             <br />
             url
-          <input type="text" value={sites[site].url} size="100"/><br />
+            <input type="text" value={sites[site].url} size="100" />
+            <br />
             selector class
-          <input type="text" value={sites[site].class} /><br/>
+            <input type="text" value={sites[site].class} />
+            <br />
           </section>
-        })}
-      </form>
-      <Link href="/">
-        <a>home</a>
-      </Link><br />
-      <Link href="/addsite">
-        <a>add a site</a>
-      </Link>
-    </div>
-  )
-}
+        );
+      })}
+    </form>
+    <Link href="/">
+      <a>home</a>
+    </Link>
+    <br />
+    <Link href="/addsite">
+      <a>add a site</a>
+    </Link>
+  </div>
+);
